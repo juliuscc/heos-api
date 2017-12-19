c1022973f33108123a1cea86492e31414bc9b2ce
Unknown <julius.cc@hotmail.com>
2017-12-19 13:17:29 +0100

    Added propper event binding and handling

--- example.js
+++ example.js
@@ -6,18 +6,7 @@ const util = require('util')
 
 const api = require('./index')
 
-function test1() {
-	console.log('test1')
-}
-
-function test2() {
-	console.log('test2')
-}
-
 api.getConnection().then(connection => {
-	api.bindEvent(connection, 'test', test1)
-	console.log(connection.events)
-	api.unbindEvent(connection, 'test', test1)
-	api.bindEvent(connection, 'test', test2)
-	console.log(connection.events)
+	api.commands.register_for_change_events(connection, 'on')
+	api.bindEvent(connection, 'player_volume_changed', console.log)
 })
