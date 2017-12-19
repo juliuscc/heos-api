b5b6baee84597f5b8cba4e6fba9a111f1d62e8e0
Unknown <julius.cc@hotmail.com>
2017-12-19 12:40:32 +0100

    Promisify all api calls
    Resolve the return value

--- example.js
+++ example.js
@@ -7,8 +7,5 @@ const util = require('util')
 const api = require('./index')
 
 api.getConnection().then(connection => {
-	// connection.events = ['test1', 'test2']
-	// console.log(connection.events)
-	api.commands.prettify_json_response(connection, 'on')
-	api.commands.register_for_change_events(connection, true)
+	api.commands.heart_beat(connection).then(console.log)
 })
