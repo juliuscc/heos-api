05e87c8650ac000e3deff400a93bd80db7e5d72f
jcelik <julius.cc@hotmail.com>
2018-12-28 16:09:07 +0100

    Repaired testing

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
 	testPathIgnorePatterns: ['/lib/', '/node_modules/'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-	collectCoverage: true,
-	mapCoverage: true
+	collectCoverage: true
 }
