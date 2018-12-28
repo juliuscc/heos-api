aee81a568ee3fc98706761e12e3d7cecd7907c2c
Unknown <julius.cc@hotmail.com>
2018-12-28 21:59:05 +0100

    Refactored heosCommand
    heosCommand could be just a function instead of a class

--- __tests__/heosCommand.ts
+++ __tests__/heosCommand.ts
@@ -1,35 +1,31 @@
-import { HeosCommand } from '../src/heosCommand'
+import { generateHeosCommand } from '../src/heosCommand'
 
 describe('Heos commands can be correctly created', () => {
 	test('A new command can be created', () => {
-		const command = new HeosCommand('system', 'heart_beat')
+		const command = generateHeosCommand('system', 'heart_beat')
 
-		expect(command.asString()).toMatch(new RegExp('^heos://'))
-		expect(command.asString()).toEqual('heos://system/heart_beat\r\n')
+		expect(command).toMatch(new RegExp('^heos://'))
+		expect(command).toEqual('heos://system/heart_beat\r\n')
 	})
 
 	test('A command can have attributes', () => {
-		const command = new HeosCommand('player', 'volume_up', {
+		const command = generateHeosCommand('player', 'volume_up', {
 			pid: 2,
 			step: 5
 		})
 
-		expect(command.asString()).toEqual(
-			'heos://player/volume_up?pid=2&step=5\r\n'
-		)
+		expect(command).toEqual('heos://player/volume_up?pid=2&step=5\r\n')
 	})
 
 	test('A command can have no attributes', () => {
-		const command = new HeosCommand('system', 'heart_beat', {})
+		const command = generateHeosCommand('system', 'heart_beat', {})
 
-		expect(command.asString()).toEqual('heos://system/heart_beat\r\n')
+		expect(command).toEqual('heos://system/heart_beat\r\n')
 	})
 
 	test('A command can have one attribute', () => {
-		const command = new HeosCommand('player', 'get_volume', { pid: 1 })
+		const command = generateHeosCommand('player', 'get_volume', { pid: 1 })
 
-		expect(command.asString()).toEqual('heos://player/get_volume?pid=1\r\n')
+		expect(command).toEqual('heos://player/get_volume?pid=1\r\n')
 	})
 })
-
-console.log(HeosCommand)
--- src/heosCommand.ts
+++ src/heosCommand.ts
@@ -7,52 +7,40 @@ export type HeosCommandAttribute = {
 	[key: string]: string | number
 }
 
-export class HeosCommand {
-	commandGroup: string
-	command: string
+function attributeString(attributes?: HeosCommandAttribute): string {
+	if (!attributes || Object.entries(attributes).length < 1) {
+		return ''
+	} else {
+		return (
+			'?' +
+			Object.entries(attributes)
+				.map(([key, value]) => {
+					if (typeof value === 'string') {
+						return `${key}='${value}'`
+					} else {
+						return `${key}=${value.toString()}`
+					}
+				})
+				.reduce((previous, current) => `${previous}&${current}`)
+		)
+	}
+}
+
+export function generateHeosCommand(
+	commandGroup: string,
+	command: string,
 	attributes?: HeosCommandAttribute
-
-	constructor(
-		commandGroup: string,
-		command: string,
-		attributes?: HeosCommandAttribute
-	) {
-		if (!commandGroup || !command) {
-			throw new Error('Missing arguments when creating HeosCommand')
-		}
-
-		this.commandGroup = commandGroup
-		this.command = command
-		this.attributes = attributes
+): string {
+	if (!commandGroup || !command) {
+		throw new Error('Missing arguments when creating HeosCommand')
 	}
 
-	private attributeString(): string {
-		if (!this.attributes || Object.entries(this.attributes).length < 1) {
-			return ''
-		} else {
-			return (
-				'?' +
-				Object.entries(this.attributes)
-					.map(([key, value]) => {
-						if (typeof value === 'string') {
-							return `${key}='${value}'`
-						} else {
-							return `${key}=${value.toString()}`
-						}
-					})
-					.reduce((previous, current) => `${previous}&${current}`)
-			)
-		}
-	}
-
-	asString(): string {
-		return [
-			prefix,
-			this.commandGroup,
-			'/',
-			this.command,
-			this.attributeString(),
-			postfix
-		].join('')
-	}
+	return [
+		prefix,
+		commandGroup,
+		'/',
+		command,
+		attributeString(attributes),
+		postfix
+	].join('')
 }
