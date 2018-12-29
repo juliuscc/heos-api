b99f6cf5d80ffed5a800059cbfe5c93a19d3f251
jcelik <julius.cc@hotmail.com>
2018-12-29 21:40:59 +0100

    Finished a basic message parser

--- __tests__/heosResponseParser.ts
+++ __tests__/heosResponseParser.ts
@@ -27,5 +27,23 @@ describe('Heos response messages can be correctly parsed', () => {
 
 		messageParser.put('{}\r\n{}\r\n')
 		expect(mockCallback.mock.calls.length).toBe(3)
+
+		messageParser.put('{}\r\n{ foo:')
+		expect(mockCallback.mock.calls.length).toBe(4)
+	})
+
+	test('The parser returns the message as an object', () => {
+		const mockCallback = jest.fn()
+
+		const messageParser: HeosResponseParser = new HeosResponseParser(
+			mockCallback
+		)
+
+		const mockObject = { foo: 'bar' }
+
+		messageParser.put(JSON.stringify(mockObject) + '\r\n')
+
+		expect(mockCallback.mock.calls.length).toBe(1)
+		expect(mockCallback.mock.calls[0][0]).toEqual(mockObject)
 	})
 })
--- src/heosResponseParser.ts
+++ src/heosResponseParser.ts
@@ -1,17 +1,34 @@
 const messageDelimiter = '\r\n'
 
 export class HeosResponseParser {
-	callback: () => any
-
-	constructor(callback: any) {
+	constructor(callback: (message: object) => void) {
 		this.callback = callback
 	}
 
+	callback: (message: object) => void
+	buffer: string = ''
+
 	put(data: string) {
-		if (data.includes(messageDelimiter)) {
-			const rows = data.split(messageDelimiter)
-			const messages = rows.filter(row => row.length > 0)
-			messages.forEach(this.callback)
+		this.buffer += data
+
+		const messages: string[] = this.buffer.split(messageDelimiter)
+		const lastMessage: string | undefined = messages.pop()
+
+		if (lastMessage === '') {
+			messages.push(lastMessage)
+			this.buffer = ''
+		} else {
+			this.buffer = lastMessage || ''
+		}
+
+		try {
+			messages
+				.filter(row => row.length > 0)
+				.map(message => JSON.parse(message))
+				.forEach(object => this.callback(object))
+		} catch {
+			console.log('Error parsing incoming messages. Flushing buffer.')
+			this.buffer = ''
 		}
 	}
 }
