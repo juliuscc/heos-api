27ddb1e85c68ba23a70c91d8eed8e300303e4ca1
jcelik <julius.cc@hotmail.com>
2019-01-13 13:49:48 +0100

    test: updated coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -7,11 +7,17 @@ module.exports = {
 	moduleFileExtensions: ['ts', 'tsx', 'js'],
 	coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
 	coverageThreshold: {
-		global: {
-			branches: 90,
-			functions: 95,
-			lines: 95,
-			statements: 95
+		'./src/listen': {
+			branches: 70,
+			functions: 75,
+			lines: 75,
+			statements: 75
+		},
+		'./src/write': {
+			branches: 70,
+			functions: 75,
+			lines: 75,
+			statements: 75
 		}
 	},
 	collectCoverageFrom: ['src/*/**.{js,ts}']
