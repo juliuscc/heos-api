b5d688e8a84bdd552c86c46a6d4a94a7ab4c0a8c
jcelik <julius.cc@hotmail.com>
2019-01-17 18:52:25 +0100

    test(discovery): Added tests for discovery

---
+++ __mocks__/dgram.ts
@@ -0,0 +1,32 @@
+let response = {
+	msg: 'urn:schemas-denon-com:device:ACT-Denon:1',
+	rinfo: { address: '192.168.0.5' }
+}
+export function __setMsg(msg: string) {
+	response.msg = msg
+}
+
+export function __setAddress(address: string) {
+	response.rinfo.address = address
+}
+
+let on = (_event: string, callback: Function) => {
+	process.nextTick(() => callback(response.msg, response.rinfo))
+}
+
+export function __setOn(newOn: any) {
+	on = newOn
+}
+
+export function createSocket() {
+	return {
+		bind: jest.fn(),
+		send: jest.fn(),
+		on,
+		close: jest.fn()
+	}
+}
+
+const dgram = { createSocket, __setMsg, __setAddress, __setOn }
+
+export default dgram
---
+++ __tests__/connection/discover.ts
@@ -0,0 +1,64 @@
+import { discoverOneDevice } from '../../src/connection/discover'
+import { mocked } from 'ts-jest/utils'
+import dgram, { createSocket } from 'dgram'
+
+jest.mock('dgram')
+
+describe('discoverOneDevice()', () => {
+	beforeAll(() => {})
+
+	test('One device can be discovered', () => {
+		expect.assertions(1)
+		return expect(discoverOneDevice()).resolves.toEqual('192.168.0.5')
+	})
+
+	test('Discover fails when no devices are found', () => {
+		expect.assertions(1)
+		// @ts-ignore
+		dgram.__setOn(() => {})
+
+		return expect(discoverOneDevice(10)).rejects.toEqual('No devices found')
+	})
+
+	test('discoverOneDevice only returns one address even if multiple devices are found', () => {
+		expect.assertions(2)
+
+		// @ts-ignore
+		dgram.__setOn((_event: string, callback: Function) => {
+			process.nextTick(() =>
+				callback('urn:schemas-denon-com:device:ACT-Denon:1', { address: '192.168.0.5' })
+			)
+			process.nextTick(() => {})
+			process.nextTick(() => {})
+			process.nextTick(() =>
+				callback('urn:schemas-denon-com:device:ACT-Denon:1', { address: '192.168.0.2' })
+			)
+		})
+
+		return discoverOneDevice().then(address => {
+			expect(address).toBe('192.168.0.5')
+			expect(address).not.toBe('192.168.0.2')
+		})
+	})
+
+	test('discoverOneDevice will only resolve a heos device', () => {
+		expect.assertions(2)
+
+		// @ts-ignore
+		dgram.__setOn((_event: string, callback: Function) => {
+			process.nextTick(() =>
+				callback('urn:schemas-denon-com:device:OTHER:2', { address: '192.168.0.5' })
+			)
+			process.nextTick(() => {})
+			process.nextTick(() => {})
+			process.nextTick(() =>
+				callback('urn:schemas-denon-com:device:ACT-Denon:1', { address: '192.168.0.2' })
+			)
+		})
+
+		return discoverOneDevice().then(address => {
+			expect(address).toBe('192.168.0.2')
+			expect(address).not.toBe('192.168.0.5')
+		})
+	})
+})
